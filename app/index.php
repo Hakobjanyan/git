b6fe0ffd97f2d47f7bd4b6de347e7e7394e40967
Hakobjanyan <ars.hakobjanyan@gmail.com>
2017-08-10 11:30:19 +0300

    Update index.php

--- app/index.php
+++ app/index.php
@@ -1,17 +1,3 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 <html>
 <head>
-    
-</head>
-<body>
-<h1>Заголовок</h1>
-    <p>параграф</p>
-<p>параграф2</p>
-
-<p>здесь я добавил изменения на ветке backend</p>>
-<p>параграф3</p>
-<!-- Комментарий -->
-<p>Первый абзац.</p>
-<p>Второй абзац.</p>
-</body>
-</html>
